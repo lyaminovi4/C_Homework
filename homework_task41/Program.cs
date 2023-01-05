428674521f3005afaebdfa83be92a7d829798426
Constantine <lyaminovi4@gmail.com>
2023-01-05 23:01:00 +0500

    add tasks 41, 43

---
+++ homework_task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write($"Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine()!);
+int[] OurArray = new int[m];
+
+void InputNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    OurArray[i] = int.Parse(Console.ReadLine()!);
+  }
+}
+
+int Compare(int[] OurArray)
+{
+  int count = 0;
+  for (int i = 0; i < OurArray.Length; i++)
+  {
+    if(OurArray[i] > 0 ) 
+    count++; 
+  }
+  return count;
+}
+InputNumbers(m);
+
+Console.WriteLine($"Введено чисел больше нуля: {Compare(OurArray)} ");
---
+++ homework_task43/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+(double, double) GetLineCoord(string N1, string N2)
+{
+    Console.Write($"Введите {N1} ");
+    double coord1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write($"Введите {N2} ");
+    double coord2 = Convert.ToDouble(Console.ReadLine());
+    return (coord1, coord2);
+}
+(double b1, double k1) = GetLineCoord("b1", "k1");
+(double b2, double k2) = GetLineCoord("b2", "k2");
+double x = (b1 - b2) / (k1 - k2) * -1;
+double y = k2 * x + b2;
+Console.WriteLine($"Точка пересечения двух прямых -> ({x},{y})");
